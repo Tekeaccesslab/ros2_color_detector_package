9a593e0ca2f71ddcded6efc329102e4fa1a28bcf
Tekalign Tesfaye <ttmengistu@aggies.ncat.edu>
2023-11-13 23:40:28 -0500

    Adding parameters from launch

--- src/cpp_launch/launch/launch.py
+++ src/cpp_launch/launch/launch.py
@@ -1,20 +1,24 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
- 
 def generate_launch_description():
     ld = LaunchDescription()
-     
+    
     color_filter=Node(
          package="green",
         #  namespace='image_color_filter', # Make sure this matches the subscriber's namespace
          executable='color_filter', # Name of the executable
-        #  name='image_subscriber' # Any name is fine
-       )
+        parameters=[
+                {"h_low": 100, "h_high":140},
+                {"s_low": 50, "s_high": 255},
+                {"v_low": 50, "v_high": 255},
+            ],
+    )
     webcam_driver= Node(
          package="orange",
         #  namespace='webcam_image_publisher', # Make sure this matches the publisher's namespace
          executable='webcam_driver', # Name of the executable
         #  name='image_publisher' # Any name is fine
+        
        )
    
     ld.add_action(color_filter)
